1515288d8b5cdfa5549a3b6356be569d7821a7e0
David Davies <David_Davies@mail.com>
2021-11-05 13:07:12 +0000

    added copy button and added the four arrays character, numberArr, lower and upper

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Start Password variable values:
+// prettier-ignore
+// Special characters
+var character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+// prettier-ignore
+// Numeric characters
+var numberArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// prettier-ignore
+// Alphabetical characters
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// prettier-ignore
+// Space is for the Uppercase conversion
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+//prettier-ignore
+var getBtn = document.querySelector("#generate");
