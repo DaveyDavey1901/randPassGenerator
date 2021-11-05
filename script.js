78deded30c440eb1c294d2d8ce6ee80f7b868418
David Davies <David_Davies@mail.com>
2021-11-05 13:15:21 +0000

    add a prompt for you to choose 1 to 4 of the options and then making sure the choice is a number and not NaN.  also added a variable that will allow you to retry x amount of times.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var tryOnceMore = 2;
 // Start Password variable values:
 // prettier-ignore
 // Special characters
@@ -13,3 +14,23 @@ var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 //prettier-ignore
 var getBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  var charChoice = Number(
+    prompt(`Please choose from the following list of options. You can choose as many as you like.
+      
+               Choose 1: for Characters.
+               Choose 2: for Numbers.
+               Choose 3: for Lowercase.
+               Choose 4: for Uppercase.
+      
+Example: 1 = Just Characters, 1234 = all options.`)
+  );
+  //this is checking the choice is a number not a letter.
+  if (isNaN(charChoice)) {
+    alert(
+      `Sorry something has gone wrong! Please choose 1,2,3 or 4 or any combination`
+    );
+    if (--tryOnceMore > 0) generatePassword();
+  }
+}
